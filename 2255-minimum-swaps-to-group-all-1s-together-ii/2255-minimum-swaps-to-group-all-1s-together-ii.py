ef66e5d3d6c2f01d6ae12e562dd440f23b09639b
Woongbi Shin <dobby12335@gmail.com>
2024-08-05 10:07:07 +0700

    Time: 656 ms (89.12%), Space: 21.3 MB (7.53%) - LeetHub

--- 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.py
+++ 2255-minimum-swaps-to-group-all-1s-together-ii/2255-minimum-swaps-to-group-all-1s-together-ii.py
@@ -3,19 +3,13 @@ class Solution:
         n = len(nums)
         window = sum(nums)
 
+        result = count = sum(nums[0:window])
         nums *= 2
-        prefix_sum = [0] * (n*2)
-        prefix_sum[0] = nums[0]
 
-        for i in range(1, n * 2):
-            prefix_sum[i] = prefix_sum[i-1] + nums[i]
-        
-        result = float('inf')
-        for i in range(n):
-            tmp_sum = prefix_sum[i + window - 1]
-            tmp_sum -= prefix_sum[i - 1] if 0 <= i - 1 else 0
-            zero_c = window - tmp_sum
-            result = min(result, zero_c)
+        for i in range(1, n):
+            count -= nums[i-1]
+            count += nums[i + window - 1]
+            result = max(result, count)
 
-        return result
+        return window - result
         
