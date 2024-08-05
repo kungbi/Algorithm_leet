e61ef219290dd3cc8adb2731f7d4f40c3d54e62a
Woongbi Shin <dobby12335@gmail.com>
2024-08-05 17:07:35 +0700

    Time: 341 ms (51.43%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,12 @@
+-- SELECT  e1.name
+-- FROM    Employee e1
+-- WHERE   5 <= (
+--     SELECT  COUNT(*)
+--     FROM    Employee e2
+--     WHERE   e1.id = e2.managerId
+-- ) 
+
 SELECT  e1.name
-FROM    Employee e1
-WHERE   5 <= (
-    SELECT  COUNT(*)
-    FROM    Employee e2
-    WHERE   e1.id = e2.managerId
-) 
+FROM    Employee e1 JOIN Employee e2 ON e1.id = e2.managerId
+GROUP BY    e1.id
+HAVING      5 <= COUNT(e1.id)
