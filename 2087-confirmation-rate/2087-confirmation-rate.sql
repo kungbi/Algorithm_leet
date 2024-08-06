dd4bb71762017f5d88ce931d87f3014548744e76
Woongbi Shin <dobby12335@gmail.com>
2024-08-06 11:08:39 +0700

    Time: 650 ms (40.98%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -1,10 +1,16 @@
-SELECT  s.user_id, ROUND(IFNULL((
-    SELECT  COUNT(*)
-    FROM    confirmations c
-    WHERE   c.action = 'confirmed' AND s.user_id = c.user_id
-) / (
-    SELECT  COUNT(*)
-    FROM    confirmations c
-    WHERE   s.user_id = c.user_id
-), 0), 2) AS confirmation_rate
-FROM    signups s
+-- SELECT  s.user_id, ROUND(IFNULL((
+--     SELECT  COUNT(*)
+--     FROM    confirmations c
+--     WHERE   c.action = 'confirmed' AND s.user_id = c.user_id
+-- ) / (
+--     SELECT  COUNT(*)
+--     FROM    confirmations c
+--     WHERE   s.user_id = c.user_id
+-- ), 0), 2) AS confirmation_rate
+-- FROM    signups s
+
+SELECT      s.user_id, ROUND(
+    AVG(IF(c.action = 'confirmed', 1, 0))
+, 2) AS 'confirmation_rate'
+FROM        signups s LEFT JOIN confirmations c ON s.user_id = c.user_id
+GROUP BY    s.user_id
