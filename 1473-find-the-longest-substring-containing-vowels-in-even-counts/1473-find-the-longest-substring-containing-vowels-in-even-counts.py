f05b0e50b176104b6bce3537be6cd7135bdac98e
Woongbi Shin <dobby12335@gmail.com>
2024-09-15 17:03:00 +0900

    Time: 405 ms (17.27%), Space: 22.1 MB (79.11%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -6,16 +6,24 @@ class Solution:
 
         mask = 0
         answer = 0
-        mem_arr = {0: -1}
-        vowels = {'a': 1, 'e': 2, 'i': 4, 'o': 8, 'u': 16}
+        mem_arr = [-1] * (2**5 + 1)
 
         for i in range(n):
-            if s[i] in vowels:
-                mask ^= vowels[s[i]]
-            if mask not in mem_arr:
+            if s[i] == 'a':
+                mask ^= 1 << 0
+            elif s[i] == 'e':
+                mask ^= 1 << 1
+            elif s[i] == 'i':
+                mask ^= 1 << 2
+            elif s[i] == 'o':
+                mask ^= 1 << 3
+            elif s[i] == 'u':
+                mask ^= 1 << 4
+            
+            if mem_arr[mask] == -1 and 0 < mask:
                 mem_arr[mask] = i
-            else:
-                answer = max(answer, i - mem_arr[mask])
+            
+            answer = max(answer, i - mem_arr[mask])
 
         return answer
             
