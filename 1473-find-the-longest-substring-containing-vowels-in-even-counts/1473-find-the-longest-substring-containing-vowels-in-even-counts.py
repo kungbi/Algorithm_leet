b3ffb07d33648abd84a43e12b14551e69746c5b4
Woongbi Shin <dobby12335@gmail.com>
2024-09-15 16:56:23 +0900

    Time: 815 ms (6.68%), Space: 22.9 MB (6.96%) - LeetHub

---
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+class Solution:
+    def findTheLongestSubstring(self, s: str) -> int:
+        n = len(s)
+
+        mask = 0
+        answer = 0
+        mem_arr = [-1] * (2**5)
+
+        for i in range(n):
+            if s[i] == 'a':
+                mask ^= 1 << 0
+            elif s[i] == 'e':
+                mask ^= 1 << 1
+            elif s[i] == 'i':
+                mask ^= 1 << 2
+            elif s[i] == 'o':
+                mask ^= 1 << 3
+            elif s[i] == 'u':
+                mask ^= 1 << 4
+            
+            print(mask)
+            if mem_arr[mask] == -1 and 0 < mask:
+                mem_arr[mask] = i
+            
+            answer = max(answer, i - mem_arr[mask])
+
+        return answer
+            
+
